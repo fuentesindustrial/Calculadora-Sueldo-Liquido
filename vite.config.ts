9ab1dae93ea658edb5f07c6762402fa764e4072b
Felipe Fuentes <fuentes.industrial@gmail.com>
2026-07-02 15:57:27 -0400

    fix: match base path casing to actual repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/calculadora-sueldo-liquido/',
+    base: '/Calculadora-Sueldo-Liquido/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
